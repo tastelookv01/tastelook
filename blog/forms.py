2208260f9703775b8c12e49b2b540d2b6ddbb46b
tastelookv01 <132771992+tastelookv01@users.noreply.github.com>
2023-05-06 21:44:31 +0530

    Add files via upload

---
+++ blog/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from .models import Post, Category
+
+
+
+
+
+class AddCategory(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ('name',)
+        
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Category'}),
+        }
+        
+        
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'author', 'sub_title', 'body', 'category', 'header_image')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Title'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'User', 'value': '', 'id': 'auth', 'type':'hidden'}),
+            # 'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'sub_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Sub-Title'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter Content'}),
+        }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'sub_title', 'body', 'category', 'header_image')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Title'}),
+            'sub_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Sub-Title'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter Content'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
